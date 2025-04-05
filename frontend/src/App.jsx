854140afbb34d1acb34497c3fd52d45461d860f2
Raish khaitu <raishkhaitu@gmail.com>
2025-04-05 00:15:51 -0700

    protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RoutePlanner from "./components/RoutePlanner";
 import LandingPage from "./pages/LandingPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 // Pages
@@ -20,16 +23,39 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/route-planner" element={<RoutePlanner />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/reset-request" element={<ResetRequest />} />
         <Route path="/reset" element={<ResetConfirm />} />
+        {/* Protected Routes */}
+        <Route
+          path="/route-planner"
+          element={
+            <PrivateRoute>
+              <RoutePlanner />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
